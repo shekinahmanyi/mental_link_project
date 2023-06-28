734e347722b0af8914829744780aa2eb94a32d94
shekinahmanyi <shekinahmanyi@gmail.com>
2023-06-28 16:36:09 +0100

    Added some other sections

--- frontend/src/pages/HowItWorks.jsx
+++ frontend/src/pages/HowItWorks.jsx
@@ -1,50 +1,41 @@
-import React from 'react';
-import Footer from '../components/Footer';
+import React from "react";
+import Footer from "../components/Footer";
+import Fade from "react-reveal/Fade";
+import h7 from "../assets/images/h7.jpg";
 
 function HowItWorks() {
-  const steps = [
-    {
-      id: 1,
-      title: "Step 1",
-      description: "Create an account on our mental health app.",
-    },
-    {
-      id: 2,
-      title: "Step 2",
-      description: "Explore our curated content, including guided meditations, stress management techniques, and helpful articles.",
-    },
-    {
-      id: 3,
-      title: "Step 3",
-      description: "Engage with our supportive community through forums and discussions.",
-    },
-    {
-      id: 4,
-      title: "Step 4",
-      description: "Track your progress and set personal goals for your mental wellness journey.",
-    },
-    // Add more steps as needed
-  ];
-
   return (
     <div>
-    <div className="pt-24 flex flex-col items-center">
-      <h2 className="text-3xl text-green-500 font-bold mb-6">How It Works</h2>
-      <div className="max-w-lg mx-auto">
-        {steps.map((step, index) => (
-          <div key={step.id} className={`flex items-start mb-6 ${index !== steps.length - 1 ? 'border-b-2 pb-4' : ''}`}>
-            <div className="flex-shrink-0">
-              <div className="h-10 w-10 rounded-full bg-green-500 flex items-center justify-center">
-                <span className="text-white font-bold">{step.id}</span>
+      <div className="pt-8 flex flex-col items-center">
+        <h1 className="text-3xl text-center font-mono text-gray-600">
+          Let's Get Started With How it works!!
+        </h1>
+        {/* SECTION 1*/}
+        <section className="bg-white py-4">
+          <div className="container mx-auto px-4 md:px-8 lg:px-16 flex flex-wrap items-center">
+            <Fade left duration={4000}>
+              <div className="w-full md:w-1/2">
+                <img src={h7} alt="Question" className="w-full h-auto" />
               </div>
-            </div>
-            <div className="ml-4">
-              <h3 className="text-lg font-semibold">{step.title}</h3>
-              <p className="text-gray-700">{step.description}</p>
-            </div>
+            </Fade>
+            <Fade right duration={4000}>
+              <div className="w-full md:w-1/2 mb-8 md:mb-0">
+                <h2 className="text-2xl font-bold mb-4 md:mb-4 font-sans">
+                  01.
+                </h2>
+                <h2 className="text-2xl text-blue-500 font-bold mb-4 md:mb-4 font-sans">
+                  Create an Account
+                </h2>
+                <p className="text-base text-gray-400">
+                  To begin, you'll need to create an account by signing in to
+                  our platform. If you already have an account, click here to
+                  login into your dashboard and then get started by exploring
+                  our resources.
+                </p>
+              </div>
+            </Fade>
           </div>
-        ))}
-      </div>
+        </section>
       </div>
       <Footer />
     </div>
