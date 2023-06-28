e4b104b42b8b5437f7204a25fb7abb94106be467
shekinahmanyi <shekinahmanyi@gmail.com>
2023-06-28 11:46:41 +0100

    Made changes on the Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,28 +1,49 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaUser, FaLink, FaBars, FaTimes } from "react-icons/fa";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
   
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
   return (
-    <nav className="navbar mb-2 shadow-lg bg-white text-neutral-content fixed top-0 inset-x-0 z-50">
+    <nav
+    className={`navbar shadow-lg bg-white text-neutral-content ${
+      isScrolled ? "fixed top-0 inset-x-0 z-50" : ""
+    }`}
+  >
       <div className="container mx-auto px-6 flex items-center justify-between">
-        <div className="px-2 text-2xl mb-6 mt-6">
+        <div className="px-5 text-2xl mb-6 mt-6">
           <span className="text-green-500 tracking-tight">
-            Mental<span className="text-gray-800">Link </span>
+            Mental<span className="text-gray-800">Link</span>
             <FaLink className="inline pr-2 md:inline-block text-3xl" />
           </span>
         </div>
-        <div className="flex items-center space-x-4 md:hidden">
+        <div className="flex items-center space-x-2 md:hidden">
           <button
             onClick={toggleMenu}
-            className="text-green-700 hover:text-green-800 text-2xl focus:outline-none"
+            className="text-green-700 hover:text-green-800 text-3xl focus:outline-none"
           >
             {isOpen ? <FaTimes /> : <FaBars />}
           </button>
@@ -30,25 +51,25 @@ function Navbar() {
         <div className="hidden md:flex md:items-center md:space-x-4">
           <Link
             to="/"
-            className="text-base px-4 py-2 font-medium text-gray-700 hover:text-gray-800"
+            className="text-lg px-4 py-2 font-medium text-gray-700 hover:text-blue-400"
           >
             Home
           </Link>
           <Link
             to="/about"
-            className="text-base px-4 py-2 font-medium text-gray-700 hover:text-gray-800"
+            className="text-lg px-4 py-2 font-medium text-gray-700 hover:text-blue-400"
           >
             About
           </Link>
           <Link
             to="/story"
-            className="text-base px-4 py-2 font-medium text-gray-700 hover:text-gray-800"
+            className="text-lg px-4 py-2 font-medium text-gray-700 hover:text-blue-400"
           >
             Our Story
           </Link>
           <Link
             to="/works"
-            className="text-base px-4 py-2 font-medium text-gray-700 hover:text-gray-800"
+            className="text-lg px-4 py-2 font-medium text-gray-700 hover:text-blue-400"
           >
             How it Works
           </Link>
