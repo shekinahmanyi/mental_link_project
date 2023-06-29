64118140568221aa415410eef22540b7c7dbf9f9
shekinahmanyi <shekinahmanyi@gmail.com>
2023-06-29 11:01:04 +0100

    designing for mobile devices

--- frontend/src/dashboard/main/Start.jsx
+++ frontend/src/dashboard/main/Start.jsx
@@ -1,14 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { FaLink, FaBars, FaTimes, FaHome, FaSearch, FaLightbulb, FaBook, FaUsers } from "react-icons/fa";
+import { FaLink, FaHome, FaSearch, FaLightbulb, FaBook, FaUsers } from "react-icons/fa";
 
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <nav className="navbar mb-2 shadow-lg bg-indigo-950 text-neutral-content fixed top-0 inset-x-0 z-50">
       <div className="container mx-auto px-6 flex items-center justify-between">
@@ -17,14 +11,6 @@ function Navbar() {
             Mental<span className="text-green-50 font-mono">Link</span>
             <FaLink className="inline pr-2 md:inline-block text-3xl" />
           </span>
-        </div>
-        <div className="flex items-center space-x-4 md:hidden">
-          <button
-            onClick={toggleMenu}
-            className="text-white hover:text-white text-2xl focus:outline-none"
-          >
-            {isOpen ? <FaTimes /> : <FaBars />}
-          </button>
         </div>
         <div className="hidden md:flex md:items-center md:space-x-4">
           <Link
@@ -64,47 +50,25 @@ function Navbar() {
           </Link>
         </div>
       </div>
-      {isOpen && (
-        <div className="container mx-auto px-6 my-4">
-          <div className="border-t border-gray-300 py-4 flex flex-col space-y-4">
-            <Link
-              to="/"
-              className="text-base font-medium text-green-50"
-            >
-              <FaHome className="inline pr-1 text-2xl" />
-              Home
-            </Link>
-            <Link
-              to="/about"
-              className="text-base font-medium text-green-50"
-            >
-              <FaLink className="inline pr-1 text-2xl" />
-              About
-            </Link>
-            <Link
-              to="/story"
-              className="text-base font-medium text-green-50"
-            >
-              <FaBook className="inline pr-1 text-2xl" />
-              Our Story
-            </Link>
-            <Link
-              to="/works"
-              className="text-base font-medium text-green-50"
-            >
-              <FaLightbulb className="inline pr-1 text-2xl" />
-              How it Works
-            </Link>
-            <Link
-              to="/community"
-              className="text-base px-4 py-2 font-medium text-green-50"
-            >
-              <FaUsers className="inline pr-1 text-2xl" />
-              Community
-            </Link>
-          </div>
+      <div className="container mx-auto px-6 my-4 md:hidden bg-indigo-950 fixed bottom-0 inset-x-0 z-50">
+        <div className="border-t border-gray-300  py-4 flex justify-between">
+          <Link to="/" className="flex flex-col items-center justify-center text-base font-medium text-green-50">
+            <FaHome className="inline text-2xl" />
+          </Link>
+          <Link to="/about" className="flex flex-col items-center justify-center text-base font-medium text-green-50">
+            <FaLink className="inline text-2xl" />
+          </Link>
+          <Link to="/story" className="flex flex-col items-center justify-center text-base font-medium text-green-50">
+            <FaBook className="inline text-2xl" />
+          </Link>
+          <Link to="/works" className="flex flex-col items-center justify-center text-base font-medium text-green-50">
+            <FaLightbulb className="inline text-2xl" />
+          </Link>
+          <Link to="/community" className="flex flex-col items-center justify-center text-base font-medium text-green-50">
+            <FaUsers className="inline text-2xl" />
+          </Link>
         </div>
-      )}
+      </div>
     </nav>
   );
 }
