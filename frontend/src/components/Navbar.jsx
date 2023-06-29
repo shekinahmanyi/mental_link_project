48fff4d62bc0bd5b220eed93e2ac1a6c375f33ab
shekinahmanyi <shekinahmanyi@gmail.com>
2023-06-29 16:10:18 +0100

    fix navbar issue

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,10 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaUser, FaLink, FaBars, FaTimes } from "react-icons/fa";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
-  
 
   useEffect(() => {
     const handleScroll = () => {
@@ -27,12 +26,16 @@ function Navbar() {
     setIsOpen(!isOpen);
   };
 
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav
-    className={`navbar bg-white text-neutral-content ${
-      isScrolled ? "fixed top-0 inset-x-0 z-50  shadow-lg" : ""
-    }`}
-  >
+      className={`navbar bg-white text-neutral-content ${
+        isScrolled ? "fixed top-0 inset-x-0 z-50  shadow-lg" : ""
+      }`}
+    >
       <div className="container mx-auto px-6 flex items-center justify-between">
         <div className="px-5 text-2xl mb-6 mt-6">
           <span className="text-green-500 tracking-tight">
@@ -51,36 +54,42 @@ function Navbar() {
         <div className="hidden md:flex md:items-center md:space-x-4">
           <Link
             to="/"
+            onClick={handleLinkClick}
             className="text-base px-4 py-2 font-medium text-gray-700 hover:text-blue-400"
           >
             Home
           </Link>
           <Link
             to="/about"
+            onClick={handleLinkClick}
             className="text-base px-4 py-2 font-medium text-gray-700 hover:text-blue-400"
           >
             About
           </Link>
           <Link
             to="/story"
+            onClick={handleLinkClick}
             className="text-base px-4 py-2 font-medium text-gray-700 hover:text-blue-400"
           >
             Our Story
           </Link>
           <Link
             to="/works"
+            onClick={handleLinkClick}
             className="text-base px-4 py-2 font-medium text-gray-700 hover:text-blue-400"
           >
             How it Works
           </Link>
           <Link
             to="/signup"
+            onClick={handleLinkClick}
             className="bg-blue-500 w-auto inline-block text-white rounded-lg text-base font-medium px-4 py-2 transition duration-300 hover:bg-blue-600"
           >
             Sign Up
           </Link>
           <Link
             to="/login"
+            onClick={handleLinkClick}
             className="border-2 border-green-500 text-green-500 rounded-lg flex items-center justify-center text-base font-medium px-4 py-2 transition duration-300 hover:bg-green-500 hover:text-white"
           >
             <FaUser className="mr-2" />
@@ -91,39 +100,44 @@ function Navbar() {
       {isOpen && (
         <div className="container mx-auto px-6 my-4">
           <div className="border-t border-gray-300 py-4 flex flex-col space-y-4">
-            
             <Link
               to="/"
-              className="text-base font-medium text-gray-700 hover:text-gray-800 "
+              onClick={handleLinkClick}
+              className="text-base font-medium text-gray-700 hover:text-gray-800"
             >
               Home
             </Link>
             <Link
               to="/about"
+              onClick={handleLinkClick}
               className="text-base font-medium text-gray-700 hover:text-gray-800"
             >
               About
             </Link>
             <Link
               to="/story"
+              onClick={handleLinkClick}
               className="text-base font-medium text-gray-700 hover:text-gray-800"
             >
               Our Story
             </Link>
             <Link
               to="/works"
+              onClick={handleLinkClick}
               className="text-base font-medium text-gray-700 hover:text-gray-800"
             >
               How it Works
             </Link>
             <Link
               to="/signup"
+              onClick={handleLinkClick}
               className="text-base font-medium text-gray-700 hover:text-gray-800"
             >
               Sign Up
             </Link>
             <Link
               to="/login"
+              onClick={handleLinkClick}
               className="text-base font-medium text-green-500 hover:text-green-600 flex items-center"
             >
               <FaUser className="mr-2" />
